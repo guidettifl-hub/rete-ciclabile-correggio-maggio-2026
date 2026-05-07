44bd753da914f9d6edd7606655a199a735eb339d
guidettifl-hub <guidettifl@gmail.com>
2026-05-07 13:51:57 +0200

    Add files via upload

---
+++ overlay.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    function add(id, html) {
+        if (!document.getElementById(id)) {
+            let el = document.createElement("div");
+            el.id = id;
+            el.innerHTML = html;
+            document.body.appendChild(el);
+        }
+    }
+
+    add("titolo-webmap", `
+        <div class="titolo-riga1">Rete ciclabile di Correggio (Reggio Emilia)</div>
+        <div class="titolo-riga2">Aggiornamento: aprile 2026</div>
+    `);
+
+    add("box-info", `
+        Clicca su un tratto o su un punto della rete per aprire la relativa scheda PDF
+    `);
+
+    add("firma-fissa", `
+        © Ing. Flavio Guidetti
+    `);
+
+});
