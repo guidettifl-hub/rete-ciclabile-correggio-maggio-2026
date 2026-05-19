b31002bb2e1a709dde098559e7b28fefa9b03cdf
guidettifl-hub <guidettifl@gmail.com>
2026-05-19 12:25:43 +0200

    Update MS_ManutenzioneStraordinaria_7.js

--- data/MS_ManutenzioneStraordinaria_7.js
+++ data/MS_ManutenzioneStraordinaria_7.js
@@ -1,1 +1,56 @@
-var json_MS_ManutenzioneStraordinaria_7 = {"type":"FeatureCollection","name":"MS_ManutenzioneStraordinaria_7","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","titolo":"MS_Manutenzione Straordinaria","via":"Veneto","tipo":"Spostare n. 6 JERSEY da v.le Veneto a via Campagnola","note":"n. 6 JERSEY presenti in v.le Veneto (devono essere un refuso di “qualcosa” che nel passato si doveva proteggere, ma che oggi non esiste più!) andrebbero trasferiti in via Campagnola come indicato nelle schede sotto.","scheda":"../ALLEGATI/PDF/MS001_MS002.pdf"},"geometry":{"type":"Point","coordinates":[10.781156557700966,44.773112402243015]}},{"type":"Feature","properties":{"fid":"2","titolo":"MS_Manutenzione Straordinaria","via":"Campagnola","tipo":"Messa in sicurezza pista ciclabile","note":"SPOSTARE VERSO NORD I JERSEY ESISTENTI, distribuendoli distanziati opportunamente lungo l’intero tratto per evidenziare (proteggere) il ciclopedonale in una strada molto trafficata e (attualmente!) priva di segnaletica orizzontale.","scheda":"../ALLEGATI/PDF/MS003_MS004.pdf"},"geometry":{"type":"Point","coordinates":[10.780622250236645,44.775958008262556]}}]}
+var json_MS_ManutenzioneStraordinaria_7 = {
+"type":"FeatureCollection",
+"name":"MS_ManutenzioneStraordinaria_7",
+"crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},
+"features":[
+
+{
+"type":"Feature",
+"properties":{
+    "fid":"1",
+    "titolo":"MS_Manutenzione Straordinaria",
+    "via":"Veneto",
+    "tipo":"Spostare n. 6 JERSEY da v.le Veneto a via Campagnola",
+    "note":"n. 6 JERSEY presenti in v.le Veneto (devono essere un refuso di “qualcosa” che nel passato si doveva proteggere, ma che oggi non esiste più!) andrebbero trasferiti in via Campagnola come indicato nelle schede sotto.",
+    "scheda":"../ALLEGATI/PDF/MS001_MS002.pdf"
+},
+"geometry":{
+    "type":"Point",
+    "coordinates":[10.781156557700966,44.773112402243015]
+}
+},
+
+{
+"type":"Feature",
+"properties":{
+    "fid":"2",
+    "titolo":"MS_Manutenzione Straordinaria",
+    "via":"Campagnola",
+    "tipo":"Messa in sicurezza pista ciclabile",
+    "note":"SPOSTARE VERSO NORD I JERSEY ESISTENTI, distribuendoli distanziati opportunamente lungo l’intero tratto per evidenziare (proteggere) il ciclopedonale in una strada molto trafficata e (attualmente!) priva di segnaletica orizzontale.",
+    "scheda":"../ALLEGATI/PDF/MS003_MS004.pdf"
+},
+"geometry":{
+    "type":"Point",
+    "coordinates":[10.780622250236645,44.775958008262556]
+}
+},
+
+{
+"type":"Feature",
+"properties":{
+    "fid":"3",
+    "titolo":"MS_Manutenzione Straordinaria",
+    "via":"Campagnola",
+    "tipo":"Messa in sicurezza pista ciclabile",
+    "note":"N. 2 JERSEY PRIMA DEL CARTELLO in linea con la segnaletica orizzontale (SCOMPARSA da ripristinare !)\nSPOSTARE VERSO NORD I JERSEY ESISTENTI, distribuendoli distanziati opportunamente lungo l’intero tratto per evidenziare (proteggere) il ciclopedonale in una strada molto trafficata e (attualmente!) priva di segnaletica orizzontale.\nDOVE SONO COLLOCATI ORA I JERSEY NON SI CAPISCE A COSA SERVANO",
+    "scheda":"../ALLEGATI/PDF/MS001_MS002.pdf"
+},
+"geometry":{
+    "type":"Point",
+    "coordinates":[10.780380,44.776127]
+}
+}
+
+]
+}
