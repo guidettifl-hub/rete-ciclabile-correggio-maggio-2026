dff76c37bf3a192329a07711cdf077e59c04c155
guidettifl-hub <guidettifl@gmail.com>
2026-06-03 18:43:12 +0200

    Update overlay.js

--- overlay.js
+++ overlay.js
@@ -30,7 +30,7 @@ add("box-info", `
     </div>
 
     <div class="box-testo"
-     style="color:#0099FF; text-align:center; font-size:15px; line-height:1.25;">
+         style="color:#0056cc; text-align:center; font-size:15px; line-height:1.25;">
     per aprire la LEGENDA<br>
     CLIC su una ICONA per aprire la scheda descrittiva<br>
     CLIC su una STRADA per aprire Street View
